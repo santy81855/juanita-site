4a97fcb3b1147bd3c96027c3faba1ea2032eba7a
santy <santy@DESKTOP-7KIGB8O>
2025-03-31 18:29:50 -0400

    updated suctions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
+import { Caveat } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
     title: "Latebis",
     description: "My art.",
 };
+
+const caveat = Caveat({
+    variable: "--font-caveat",
+});
 
 export default function RootLayout({
     children,
@@ -13,7 +18,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body className={caveat.className}>{children}</body>
         </html>
     );
 }
--- src/features/gallery/components/grid-gallery/GridGallery.tsx
+++ src/features/gallery/components/grid-gallery/GridGallery.tsx
@@ -1,6 +1,6 @@
 "use client";
 import * as SanityTypes from "@/@types";
-import Masonry from "react-responsive-masonry";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import Image from "next/image";
 import styles from "./style.module.css";
 import { useEffect, useState } from "react";
@@ -23,7 +23,7 @@ type GridGalleryProps = {
 const GridGallery = ({ images }: GridGalleryProps) => {
     //const [isLoading, setIsLoading] = useState(true);
     const [numColumns, setNumColumns] = useState(3);
-    const [curCategory, setCurCategory] = useState("All");
+    const [curCategory, setCurCategory] = useState("Drawings");
     const [itemHovered, setItemHovered] = useState(-1);
     const [loadedImages, setLoadedImages] = useState<{
         [url: string]: boolean;
@@ -37,27 +37,27 @@ const GridGallery = ({ images }: GridGalleryProps) => {
     };
 
     // create useEffect that will change isLoading to false after 2 seconds
-    useEffect(() => {
-        // setTimeout(() => {
-        //     setIsLoading(false);
-        // }, 2000);
-        // set up a listener for the resize event
-        function handleResize() {
-            if (window.innerWidth < 768) {
-                setNumColumns(3);
-            } else if (window.innerWidth < 1024) {
-                setNumColumns(4);
-            } else {
-                setNumColumns(5);
-            }
-        }
-        // add the event listener
-        window.addEventListener("resize", handleResize);
-        // remove the event listener
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
+    // useEffect(() => {
+    //     // setTimeout(() => {
+    //     //     setIsLoading(false);
+    //     // }, 2000);
+    //     // set up a listener for the resize event
+    //     function handleResize() {
+    //         if (window.innerWidth < 768) {
+    //             setNumColumns(3);
+    //         } else if (window.innerWidth < 1024) {
+    //             setNumColumns(4);
+    //         } else {
+    //             setNumColumns(5);
+    //         }
+    //     }
+    //     // add the event listener
+    //     window.addEventListener("resize", handleResize);
+    //     // remove the event listener
+    //     return () => {
+    //         window.removeEventListener("resize", handleResize);
+    //     };
+    // }, []);
 
     const handleCategory = (category: string) => {
         setCurCategory(category);
@@ -66,12 +66,6 @@ const GridGallery = ({ images }: GridGalleryProps) => {
     return (
         <section className={styles.container}>
             <section className={styles.buttonContainer}>
-                <button
-                    className={styles.button1}
-                    onClick={() => handleCategory("All")}
-                >
-                    All
-                </button>
                 <button
                     className={styles.button2}
                     onClick={() => handleCategory("Drawings")}
@@ -86,56 +80,59 @@ const GridGallery = ({ images }: GridGalleryProps) => {
                 </button>
             </section>
             <section className={styles.imageContainer}>
-                <Masonry columnsCount={numColumns} gutter="10px">
-                    {images.map(
-                        (image, i) =>
-                            (curCategory === "All" ||
-                                curCategory === image.category) && (
-                                <motion.div
-                                    key={i}
-                                    initial={{ opacity: 0 }}
-                                    whileInView={{ opacity: 1 }}
-                                    transition={{ duration: 0 }}
-                                    viewport={{ once: true, amount: 0.5 }}
-                                    onMouseEnter={() => setItemHovered(i)}
-                                    onMouseLeave={() => setItemHovered(-1)}
-                                    className={`${
-                                        itemHovered === i
-                                            ? styles.hovered
-                                            : styles.notHovered
-                                    } ${styles.imageWrapper} ${
-                                        loadedImages[image.src]
-                                            ? styles.loaded
-                                            : styles.notLoaded
-                                    }`}
-                                >
-                                    <div
-                                        className={`${styles.caption} ${itemHovered === i && styles.captionHovered}`}
-                                    >
-                                        {image.title}
-                                    </div>
-                                    <Image
+                <ResponsiveMasonry
+                    columnsCountBreakPoints={{ 768: 3, 1024: 4 }}
+                >
+                    <Masonry columnsCount={numColumns} gutter="10px">
+                        {images.map(
+                            (image, i) =>
+                                curCategory === image.category && (
+                                    <motion.div
                                         key={i}
-                                        src={image.src}
-                                        style={{
-                                            width: "100%",
-                                            height: "auto",
-                                            display: "block",
-                                        }}
-                                        width={image.width / 5}
-                                        height={image.height / 5}
-                                        alt={image.alt}
-                                        placeholder="blur"
-                                        blurDataURL={image.blurDataURL}
-                                        quality={40}
-                                        onLoad={() =>
-                                            handleImageLoad(image.src)
-                                        }
-                                    />
-                                </motion.div>
-                            )
-                    )}
-                </Masonry>
+                                        initial={{ opacity: 0 }}
+                                        whileInView={{ opacity: 1 }}
+                                        transition={{ duration: 0 }}
+                                        viewport={{ once: true, amount: 0.5 }}
+                                        onMouseEnter={() => setItemHovered(i)}
+                                        onMouseLeave={() => setItemHovered(-1)}
+                                        className={`${
+                                            itemHovered === i
+                                                ? styles.hovered
+                                                : styles.notHovered
+                                        } ${styles.imageWrapper} ${
+                                            loadedImages[image.src]
+                                                ? styles.loaded
+                                                : styles.notLoaded
+                                        }`}
+                                    >
+                                        <div
+                                            className={`${styles.caption} ${itemHovered === i && styles.captionHovered}`}
+                                        >
+                                            {image.title}
+                                        </div>
+                                        <Image
+                                            key={i}
+                                            src={image.src}
+                                            style={{
+                                                width: "100%",
+                                                height: "auto",
+                                                display: "block",
+                                            }}
+                                            width={image.width / 5}
+                                            height={image.height / 5}
+                                            alt={image.alt}
+                                            placeholder="blur"
+                                            blurDataURL={image.blurDataURL}
+                                            quality={40}
+                                            onLoad={() =>
+                                                handleImageLoad(image.src)
+                                            }
+                                        />
+                                    </motion.div>
+                                )
+                        )}
+                    </Masonry>
+                </ResponsiveMasonry>
             </section>
         </section>
     );
